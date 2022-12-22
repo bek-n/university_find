9560b54915f780e888d7de09d3a8147771916b15
Bek <nematjanov95@gmail.com>
2022-12-22 22:01:55 +0500

    Adding page

---
+++ lib/Home/add_country.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AddingCountry extends StatefulWidget {
+  const AddingCountry({super.key});
+
+  @override
+  State<AddingCountry> createState() => _AddingCountryState();
+}
+
+class _AddingCountryState extends State<AddingCountry> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
